b781c370d5833faf933894e38dac09867f7a3832
Idanturjeman <idan_turjeman29@walla.co.il>
2022-05-16 14:35:56 +0300

    some fixes

--- sources/OrgChart.hpp
+++ sources/OrgChart.hpp
@@ -13,43 +13,35 @@ namespace ariel
         string name;
         vector<Node> sones;
     };
-
     class OrgChart
-    {
-    private:
-        Node root;
-        bool find_sub(Node &node, string &father, string &son);
-        void fill_q_for_level_order(Node &node);
-        // void fill_q_for_print_level_order(Node &node);
-        void fill_q_for_reverse_order(Node &node);
-        void fill_q_for_preorder(Node &node);
-        vector<string> b_level_order;
-        // vector<string> b_print_level_order;
-        vector<string> b_reverse_order;
-        vector<string> b_preorder;
-
+    { 
     public:
         OrgChart();
+        ~OrgChart();
         OrgChart &add_root(string name);
         OrgChart &add_sub(string father, string son);
-
         friend ostream &operator<<(ostream &out,OrgChart &root);
         string* begin_level_order();
-        string* end_level_order(); 
-        // string* begin_print_level_order();
-        // string* end_print_level_order(); 
-        string* begin_reverse_order();
-        string* reverse_order();
         string* begin_preorder();
         string* end_preorder();
+        string* end_level_order(); 
+        string* begin_reverse_order();
+        string* reverse_order();
         string* begin(){
             return begin_level_order();
-            }
+        }
         string* end(){
             return end_level_order();
-            }
-
-        ~OrgChart();
-        };
-
+        }
+    private:
+        Node root;
+        vector<string> b_level_order;
+        vector<string> b_reverse_order;
+        vector<string> b_preorder;
+        bool find_sub(Node &node, string &father, string &son);
+        void fill_q_for_level_order(Node &node);
+        void fill_q_for_reverse_order(Node &node);
+        void fill_q_for_preorder(Node &node);
+    };
+    
 }
